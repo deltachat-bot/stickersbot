3a39ef8fd51644e5290b59ea3b515ea7b8bb3b53
adbenitez <asieldbenitez@gmail.com>
2021-08-04 21:15:00 -0400

    move requirements to folder

--- setup.py
+++ setup.py
@@ -5,6 +5,21 @@ import re
 
 from setuptools import find_packages, setup
 
+
+def load_requirements(path: str) -> list:
+    """Load requirements from the given relative path."""
+    with open(path, encoding="utf-8") as file:  # noqa
+        requirements = []
+        for line in file.read().split("\n"):
+            if line.startswith("-r"):
+                dirname = os.path.dirname(path)
+                filename = line.split(maxsplit=1)[1]
+                requirements.extend(load_requirements(os.path.join(dirname, filename)))
+            elif line and not line.startswith("#"):
+                requirements.append(line.replace("==", ">="))
+        return requirements
+
+
 if __name__ == "__main__":
     MODULE_NAME = "simplebot_stickers"
     DESC = "Sticker packs for all your Delta Chat needs (SimpleBot plugin)"
@@ -14,19 +29,6 @@ if __name__ == "__main__":
         long_description = fh.read()
     with open("CHANGELOG.rst") as fh:
         long_description += "\n" + fh.read()
-
-    with open("requirements.txt", encoding="utf-8") as req:
-        install_requires = [
-            line.replace("==", ">=")
-            for line in req.read().split("\n")
-            if line and not line.startswith(("#", "-"))
-        ]
-    with open("requirements-test.txt", encoding="utf-8") as req:
-        test_deps = [
-            line.replace("==", ">=")
-            for line in req.read().split("\n")
-            if line and not line.startswith(("#", "-"))
-        ]
 
     setup(
         name=MODULE_NAME,
@@ -56,8 +58,11 @@ if __name__ == "__main__":
         zip_safe=False,
         include_package_data=True,
         packages=find_packages(),
-        install_requires=install_requires,
-        extras_require={"test": test_deps},
+        install_requires=load_requirements("requirements/requirements.txt"),
+        extras_require={
+            "test": load_requirements("requirements/requirements-test.txt"),
+            "dev": load_requirements("requirements/requirements-dev.txt"),
+        },
         entry_points={
             "simplebot.plugins": "{0} = {0}".format(MODULE_NAME),
         },
